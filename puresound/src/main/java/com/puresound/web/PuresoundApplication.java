bfcfcdeb7cffff142ceef4c1cd85ab1960ff643d
Alpha Lu <alphaluf@hotmail.com>
2020-01-07 22:41:22 -0500

    PureSound initial project skeleton.

---
+++ puresound/src/main/java/com/puresound/web/PuresoundApplication.java
@@ -0,0 +1,13 @@
+package com.puresound.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuresoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuresoundApplication.class, args);
+	}
+
+}
---
+++ puresound/src/test/java/com/puresound/web/PuresoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puresound.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuresoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
