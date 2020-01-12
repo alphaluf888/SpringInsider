179175e52f8bad8d28f34e39772d78a4209f8ed3
Alpha Lu <alphaluf@outlook.com>
2020-01-11 23:45:03 -0500

    learn java 9 collection factories. list.of; set.of; map.of. Map.entry

---
+++ puresound/src/main/java/com/puresound/web/QuickSort.java
@@ -0,0 +1,24 @@
+package com.puresound.web;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public class QuickSort {
+	public int[] qSort(int[] arrays) {
+		if(arrays.length < 2) {
+			return arrays;
+		}else {
+			final int pivot = arrays[0];
+			int[] less;
+			int[] remaining = Arrays.copyOfRange(arrays, 1, arrays.length);
+			for(int i : remaining) {
+				
+			}
+			
+			Set<String> friends = Set.of("Raphael", "Olivia", "Thibaut");
+			friends.add("abc");
+								
+			
+		}
+	}
+}
