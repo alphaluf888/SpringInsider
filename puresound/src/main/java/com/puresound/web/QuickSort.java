38e147101e9747728420f775c7a048c5abfb15bf
Alpha Lu <alphaluf@outlook.com>
2020-01-16 21:45:47 -0500

    practice java 8 streams.

--- puresound/src/main/java/com/puresound/web/QuickSort.java
+++ puresound/src/main/java/com/puresound/web/QuickSort.java
@@ -1,24 +1,20 @@
 package com.puresound.web;
 
 import java.util.Arrays;
-import java.util.Set;
+import java.util.List;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
 
 public class QuickSort {
-	public int[] qSort(int[] arrays) {
-		if(arrays.length < 2) {
-			return arrays;
-		}else {
-			final int pivot = arrays[0];
-			int[] less;
-			int[] remaining = Arrays.copyOfRange(arrays, 1, arrays.length);
-			for(int i : remaining) {
-				
-			}
-			
-			Set<String> friends = Set.of("Raphael", "Olivia", "Thibaut");
-			friends.add("abc");
-								
-			
-		}
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("str1", "str2", "str3");
+		
+		
+		List<String> finalList = names.stream().filter(a -> a.length() > 2)
+					.limit(2)
+					.collect(toList());
+		
+		finalList.forEach(System.out::println);
+					
 	}
 }
